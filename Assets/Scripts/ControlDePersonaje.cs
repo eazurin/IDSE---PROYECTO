cdaa883746463c8c36092cd2e28d291e8d4aafca
eazurin <eazurin@unsa.edu.pe>
2024-12-07 18:59:46 -0500

    Creando escena 1 y 2

--- Assets/Scripts/ControlDePersonaje.cs
+++ Assets/Scripts/ControlDePersonaje.cs
@@ -1,13 +1,18 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ControlDePersonaje : MonoBehaviour
 {
-    Rigidbody rigidbody; 
-    Transform transform;
-    AudioSource audiosource;
+    // Variables generales
+    private Rigidbody rigidbody; 
+    private Transform transform;
+    private AudioSource audiosource;
+
+    // Variables específicas para la escena 1
+    private int combustibleRecolectado = 0;
+    private int recursosRecolectados = 0;
 
     void Start()
     {
@@ -16,7 +21,6 @@ public class ControlDePersonaje : MonoBehaviour
         audiosource = GetComponent<AudioSource>();
     }
 
-   
     void Update()
     {
         ProcesarInput();
@@ -25,7 +29,6 @@ public class ControlDePersonaje : MonoBehaviour
     private void ProcesarInput()
     {
         Propulsion();
-
         Rotacion();
     }
 
@@ -33,7 +36,6 @@ public class ControlDePersonaje : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            // print("Propulsor...");
             rigidbody.AddRelativeForce(Vector3.up);
 
             if (!audiosource.isPlaying)
@@ -52,31 +54,68 @@ public class ControlDePersonaje : MonoBehaviour
         if (Input.GetKey(KeyCode.D))
         {
             var rotarDerecha = transform.rotation;
-            rotarDerecha.z -= Time.deltaTime *1;
+            rotarDerecha.z -= Time.deltaTime * 1;
             transform.rotation = rotarDerecha;
         }
         else if (Input.GetKey(KeyCode.A))
         {
             var rotarIzquierda = transform.rotation;
-            rotarIzquierda.z += Time.deltaTime *1;
+            rotarIzquierda.z += Time.deltaTime * 1;
             transform.rotation = rotarIzquierda;
         }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
+        // Gestión para ambas escenas
         switch (collision.gameObject.tag)
         {
-        case "ColisionSegura":
-            print("Bien jugado!");
-            break;
+            case "ColisionSegura":
+                if (SceneManager.GetActiveScene().buildIndex == 0)
+                {
+                    SceneManager.LoadScene(1); // Pasar a escena 1
+                }
+                else if (SceneManager.GetActiveScene().buildIndex == 1)
+                {
+                    // Solo avanzar si se han recolectado los requisitos
+                    if (combustibleRecolectado >= 2 && recursosRecolectados >= 1)
+                    {
+                        Debug.Log("¡Requisitos cumplidos! Pasando a la siguiente escena...");
+                        SceneManager.LoadScene(2); // Pasar a la siguiente escena
+                    }
+                    else
+                    {
+                        Debug.Log("Aún no has recolectado el combustible y los recursos necesarios.");
+                    }
+                }
+                break;
 
-        case "ColisionPeligrosa":
-            print("Muerto!");
-            break;
+            case "ColisionPeligrosa":
+                // Reiniciar escena actual
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+                break;
+
+            case "Combustible":
+                if (SceneManager.GetActiveScene().buildIndex == 1)
+                {
+                    combustibleRecolectado++;
+                    Destroy(collision.gameObject); // Eliminar el objeto recolectado
+                    Debug.Log($"Combustible recolectado: {combustibleRecolectado}");
+                }
+                break;
+
+            case "Recurso":
+                if (SceneManager.GetActiveScene().buildIndex == 1)
+                {
+                    recursosRecolectados++;
+                    Destroy(collision.gameObject); // Eliminar el objeto recolectado
+                    Debug.Log($"Recursos recolectados: {recursosRecolectados}");
+                }
+                break;
+
+            default:
+                Debug.Log("Colisión con un objeto no identificado");
+                break;
         }
-    
     }
-
 }
-
